4180b705bce91fa313ae6c64fd17be7477d20a96
windbeside <windbeside>
2015-06-18 11:29:55 +0800

    添加配置文件

---
+++ src/www/config_default.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*- 
+'''
+Created on 2015年5月11日
+
+@author: sayoko
+'''
+configs = {
+           'db' : {
+                   'host' : '172.0.0.1',
+                   'port' : 3306,
+                   'user' : 'root',
+                   'password' : 'root',
+                   'database' : 'awesome'
+                   },
+           'session' : {
+                        'secret' : 'AwEsOmE'
+                        }
+           }
---
+++ src/www/config_override.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*- 
+'''
+Created on 2015年5月11日
+
+@author: sayoko
+'''
+configs = {
+           'db' : {
+                   'host' : ''
+                   }
+           }
